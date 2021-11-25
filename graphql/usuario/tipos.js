c3f2153697a4bc2ff5e7cafaa03f6679fbf27e3d
LuisGuerra <lcgg18@gmail.com>
2021-11-25 14:13:57 -0500

    tipos de usuarios en graphql

---
+++ graphql/usuario/tipos.js
@@ -0,0 +1,43 @@
+const { gql } = require ('apollo-server-express');
+
+const tiposUsuario = gql`
+  type Usuario {
+    _id: ID!
+    nombre: String!
+    apellido: String!
+    identificacion: String!
+    correo: String!
+    rol: Enum_Rol!
+    estado: Enum_EstadoUsuario
+  }
+
+  type Query {
+    Usuarios: [Usuario]
+    Usuario(_id: String!): Usuario
+    Estudiantes(rol: String!): [Usuario]
+  }
+
+  type Mutation {
+    crearUsuario(
+      nombre: String!
+      apellido: String!
+      identificacion: String!
+      correo: String!
+      rol: Enum_Rol!
+      estado: Enum_EstadoUsuario
+    ): Usuario
+
+    editarUsuario(
+      _id: String!
+      nombre: String!
+      apellido: String!
+      identificacion: String!
+      correo: String!
+      estado: Enum_EstadoUsuario!
+    ): Usuario
+
+    eliminarUsuario(_id: String, correo: String): Usuario
+  }
+`;
+
+module.exports = { tiposUsuario };
