cdd28dc6dab42e3d1d906896012962e2e3a227e0
JulianPadilla <90355351+padiljul@users.noreply.github.com>
2021-11-26 17:15:10 -0500

    creación inicial tipo proyecto

--- graphql/proyectos/tipos.js
+++ graphql/proyectos/tipos.js
@@ -1,1 +1,19 @@
+const { gql } = require ('apollo-server-express');
 
+const tiposProyectos = gql`
+  type Proyecto{
+    _id: ID!
+    nombre: String!
+    appearsIn: [objetivos!]!
+    presupuesto: Int!
+    lider: 
+    fechaInicio: Date!          //falta crear resolver Date
+    fechaFin: Date!
+    estado: Enum_Estadop!
+    fase: Enum_Fasep!
+  }
+
+
+`;
+
+module.exports = { tiposProyectos };
