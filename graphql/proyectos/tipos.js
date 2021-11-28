238a7a428a2d02af557d785324632ca8d70358fb
JulianPadilla <90355351+padiljul@users.noreply.github.com>
2021-11-28 16:34:18 -0500

    Update tipos.js

--- graphql/proyectos/tipos.js
+++ graphql/proyectos/tipos.js
@@ -6,13 +6,47 @@ const tiposProyectos = gql`
     nombre: String!
     appearsIn: [objetivos!]!
     presupuesto: Int!
-    lider: 
+    lider: Enum_Usuario!
     fechaInicio: Date!          //falta crear resolver Date
     fechaFin: Date!
     estado: Enum_Estadop!
     fase: Enum_Fasep!
   }
 
+  type Query {
+    Proyectos: [Proyecto]
+    Proyecto(_id: String!): Proyecto
+    Proyectos(estado: String!): [Estados]
+    proyectos(fase: String!): [Fase]
+  }
+  
+   type Mutation {
+   
+    crearProyecto(
+      nombre: String!
+      objetivos: [objetivos!]
+      presupuesto: Int
+      lider: Enum_usuario!
+      fechainicio: Date!
+      fechaFin: Date!
+      estado: Enum_Estadop!
+      fase: Enum_Fasep!
+    ): Usuario
+    
+    editarProyecto(
+      _id: String!
+      nombre: String!
+      objetivos: [objetivos!]
+      presupuesto: Int
+      lider: Enum_usuario!
+      fechainicio: Date!
+      fechaFin: Date!
+      estado: Enum_Estadop!
+      fase: Enum_Fasep!
+    ): Usuario
+    
+    eliminarProyecto(_id: String): Proyecto
+  }
 
 `;
 
