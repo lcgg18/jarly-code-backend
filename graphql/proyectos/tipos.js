0f35c0ae3d01387364ab90ddbaacc7dec51649ce
JulianPadilla <90355351+padiljul@users.noreply.github.com>
2021-11-29 11:04:50 -0500

    Update tipos.js

--- graphql/proyectos/tipos.js
+++ graphql/proyectos/tipos.js
@@ -1,48 +1,54 @@
 const { gql } = require ('apollo-server-express');
 
 const tiposProyectos = gql`
+
+  type objetivo{
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
+
   type Proyecto{
     _id: ID!
     nombre: String!
-    appearsIn: [objetivos!]!
+    objetivos: [objetivo]
     presupuesto: Int!
-    lider: Enum_Usuario!
-    fechaInicio: Date!          //falta crear resolver Date
+    lider: Usuario
+    fechaInicio: Date!
     fechaFin: Date!
-    estado: Enum_Estadop!
-    fase: Enum_Fasep!
+    estado: Enum_EstadoProyecto!
+    fase: Enum_FaseProyecto!
   }
 
   type Query {
     Proyectos: [Proyecto]
     Proyecto(_id: String!): Proyecto
     Proyectos(estado: String!): [Estados]
-    proyectos(fase: String!): [Fase]
+    Proyectos(fase: String!): [Fase]
   }
   
    type Mutation {
    
     crearProyecto(
       nombre: String!
-      objetivos: [objetivos!]
+      objetivos: [objetivo]
       presupuesto: Int
-      lider: Enum_usuario!
+      lider: Usuario!
       fechainicio: Date!
       fechaFin: Date!
-      estado: Enum_Estadop!
-      fase: Enum_Fasep!
+      estado: Enum_EstadoProyecto!
+      fase: Enum_FaseProyecto!
     ): Usuario
     
     editarProyecto(
       _id: String!
       nombre: String!
-      objetivos: [objetivos!]
+      objetivos: [objetivo]
       presupuesto: Int
-      lider: Enum_usuario!
+      lider: Usuario!
       fechainicio: Date!
       fechaFin: Date!
-      estado: Enum_Estadop!
-      fase: Enum_Fasep!
+      estado: Enum_EstadoProyecto!
+      fase: Enum_FaseProyecto!
     ): Usuario
     
     eliminarProyecto(_id: String): Proyecto
